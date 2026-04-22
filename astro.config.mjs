b02aeb5283006afceaa31580846723bf44322e65
Mila Massaro <152287537+misama17@users.noreply.github.com>
2026-04-22 11:05:54 +0200

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import node from '@astrojs/node'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: node({ mode: 'standalone' }),
+})
